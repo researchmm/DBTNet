5a8876eff3a413f2f6b2b2e21798b828a5ca42a1
root <root@container-e2238-1569986296054-52465-02-000003.rr1.philly.selfhost.corp.microsoft.com>
2020-01-03 18:06:47 +0800

    update

---
+++ data/README.md
@@ -0,0 +1,10 @@
+download the data via:
+wget https://australiav100data.blob.core.windows.net/heliang/imagenet_train.rec
+wget https://australiav100data.blob.core.windows.net/heliang/imagenet_train.idx
+wget https://australiav100data.blob.core.windows.net/heliang/imagenet_val.rec
+wget https://australiav100data.blob.core.windows.net/heliang/imagenet_val.idx
+mv imagenet_* imagenet/
+
+wget https://australiav100data.blob.core.windows.net/heliang/cub.tar
+tar -xvf cub.tar
+
--- train_imagenet_dbt.sh
+++
@@ -1,9 +0,0 @@
-python3 train_imagenet_dbt.py \
-  --rec-train ../../data/imagenet/val.rec --rec-train-idx ../../data/imagenet/val.idx \
-  --rec-val ../../data/imagenet/val.rec --rec-val-idx ../../data/imagenet/val.idx \
-  --model resnet50  --mode hybrid \
-  --lr 0.4 --lr-mode cosine --num-epochs 120 --batch-size 128 --num-gpus 8 -j 60 \
-  --warmup-epochs 5 --dtype float16 \
-  --use-rec --no-wd --label-smoothing --last-gamma \
-  --save-dir ../../model/params_imagenet_dbt \
-  --logging-file ../../model/log/imagenet_dbt.log \
